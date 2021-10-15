d8e8c2eb8fb5ea0ef8ff7957b7f98614bdfc5d64
moise10r <moiserushanika2006@gmail.com>
2021-10-15 15:26:17 +0200

    Add the second factorial test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,5 +15,9 @@ describe Solver do
     it 'returns the factorial of 6' do
       expect(@solver.factorial(6)).to eq 720
     end
+
+    it 'returns the factorial of 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
   end
 end
