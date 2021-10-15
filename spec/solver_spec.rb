6bd9b0730cc8f7544dc651b1e50e5ead79580db8
moise10r <moiserushanika2006@gmail.com>
2021-10-15 15:23:01 +0200

    Add test for creating a new instance

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,4 +4,11 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
+
+  describe '#new' do
+    it 'returns a Solver object' do
+      expect(@solver).to be_an_instance_of Solver
+    end
+  end
+
 end
