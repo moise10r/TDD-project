6acbc7337e53770d826bf8865246d7be608a3edc
moise10r <moiserushanika2006@gmail.com>
2021-10-15 15:30:46 +0200

    Add the reverse string test.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,4 +20,10 @@ describe Solver do
       expect(@solver.factorial(0)).to eq 1
     end
   end
+
+  describe '#reverse' do
+    it 'returns the reverse of hello' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+    end
+  end
 end
