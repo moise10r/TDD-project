62e560dfbf759a840641b43ca90ba74eabf103f4
Akasha <Akasha.Rojee@gmail.com>
2021-10-15 20:59:25 +0400

    Add code for factorial

---
+++ solver.rb
@@ -0,0 +1,16 @@
+class Solver
+
+  def factorial(N)
+    raise StandardError if N.negative?
+
+    result = 1
+
+    while N.positive?
+      result *= num
+      num -= 1
+    end
+
+    result
+  end
+
+end
