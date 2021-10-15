ab8e66e606a4386c511b3ed20c630e46f4d92c60
Akasha <Akasha.Rojee@gmail.com>
2021-10-15 20:54:59 +0400

    Add factorial test for negative integer

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,13 +12,18 @@ describe Solver do
   end
 
   describe '#factorial' do
+    it 'returns the factorial of 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+    
     it 'returns the factorial of 6' do
       expect(@solver.factorial(6)).to eq 720
     end
 
-    it 'returns the factorial of 0' do
-      expect(@solver.factorial(0)).to eq 1
+    it 'raises exception for negative integer' do
+      expect { @solver.factorial(-5) }.to raise_exception an_instance_of(StandardError)
     end
+
   end
 
   describe '#reverse' do
